95f419130356e2eb5afd4a7025a7c9ce19cbdcc7
Felipe-Rod91 <felipe.git@outlook.com>
2023-11-09 22:35:07 -0300

    atualizando mais exercícios

--- Módulo 1/02 - Tratando dados e fazendo contas/ex005 - Antecessor e sucessor.py
+++ Módulo 1/02 - Tratando dados e fazendo contas/ex005 - Antecessor e sucessor.py
@@ -1,3 +1,4 @@
+# programa para mostrar o antecessor e o sucessor de um número qualquer.
+
 n = int(input('Digite um número:'))
-print('O antecessor do número {} é {}, e seu sucessor é {}.'.format(n, n - 1, n + 1))
-#programa para mostrar o antecessor e o sucessor de um número qualquer
+print(f'O antecessor de {n} é {n - 1}, e o seu sucessor é {n + 1}.')
--- Módulo 1/02 - Tratando dados e fazendo contas/ex006 - Dobro, triplo e raiz quadrada.py
+++ Módulo 1/02 - Tratando dados e fazendo contas/ex006 - Dobro, triplo e raiz quadrada.py
@@ -1,3 +1,4 @@
-n4 = int(input('Digite outro número:'))
-print('O dobro do número {} é {}, o seu triplo é {} e sua raíz quadrada é {:.2f}'.format(n4, n4 * 2, n4 * 3, n4 ** (1/2)))
 #programa para mostrar o dobro, triplo e a raíz quadrada de um número qualquer
+
+num = int(input('Digite um número: '))
+print(f'O dobro de {num} é {num * 2}, o seu triplo é {num * 3} e sua raiz quadrada é {num ** (1/2)}.')
