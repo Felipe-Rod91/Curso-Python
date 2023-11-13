86001639b158493bb4117b3c2ed9e45ad9fb8fa0
Felipe-Rod91 <felipe.git@outlook.com>
2023-11-12 22:20:02 -0300

    Começo dos exercícios da aula 4
    Fazendo as alterações na sintaxe dos exercícios 28 e 29 da aula 4

--- Módulo 1/04 - Condições - Parte 1 (if, else)/ex028 - Jogo da adivinhação 1.0.py
+++ Módulo 1/04 - Condições - Parte 1 (if, else)/ex028 - Jogo da adivinhação 1.0.py
@@ -1,14 +1,22 @@
+# Escreva um programa que faça o computador “pensar” em um número inteiro entre 1 e 5 e peça para o usuário tentar
+# descobrir qual foi o número escolhido pelo computador. O programa deverá escrever na tela se o usuário venceu
+# ou perdeu.
+
 from random import randint
 from time import sleep
+
 computador = randint(1, 5)
+
 print('Estou pensando num número entre 1 e 5...')
+
 sleep(2)
+
 num = int(input('Em qual número estou pensando? '))
 print('PROCESSANDO...')
+
 sleep(3)
+
 if num == computador:
     print('Parabéns! Era exatamente esse número =D')
 else:
     print('ERROOOOUUU! Eu tava pensando no {}.'.format(computador))
-#Escreva um programa que faça o computador “pensar” em um número inteiro entre 1 e 5 e peça para o usuário tentar
-#descobrir qual foi o número escolhido pelo computador. O programa deverá escrever na tela se o usuário venceu ou perdeu.
--- Módulo 1/04 - Condições - Parte 1 (if, else)/ex029 - Radar eletrônico.py
+++ Módulo 1/04 - Condições - Parte 1 (if, else)/ex029 - Radar eletrônico.py
@@ -1,9 +1,11 @@
+# Escreva um programa que leia a velocidade de um carro. Se ele ultrapassar 80Km/h, mostre uma mensagem dizendo que ele
+# foi multado. A multa vai custar R$7,00 por cada Km acima do limite.
+
 vel = int(input('Qual a velocidade do carro em km/h? '))
 print('Calculando...')
+
 if vel > 80:
     print('Você ultrapassou a velocidade máxima permitida em {}km/h.'.format(vel - 80))
     print('Sua multa é de {:.2f} reais'.format((vel - 80) * 7))
 else:
     print('Você estava dentro do limite de velocidade permitido. Boa viagem!')
-#Escreva um programa que leia a velocidade de um carro. Se ele ultrapassar 80Km/h, mostre uma mensagem dizendo que ele
-#foi multado. A multa vai custar R$7,00 por cada Km acima do limite.
