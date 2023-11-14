65d1fca8996f55f79bdf95a973e533a552746308
Felipe-Rod91 <felipe.git@outlook.com>
2023-11-13 22:40:37 -0300

    terminado o módulo 1 inteiro

--- Módulo 1/04 - Condições - Parte 1 (if, else)/ex033 - Maior e menor valores.py
+++ Módulo 1/04 - Condições - Parte 1 (if, else)/ex033 - Maior e menor valores.py
@@ -1,6 +1,9 @@
+# Faça um programa que leia três números e mostre qual é o maior e qual é o menor.
+
 a = int(input('Digite um número: '))
 b = int(input('Digite o segundo número: '))
 c = int(input('Digite o terceiro número: '))
+
 menor = a
 if b < a and b < c:
     menor = b
@@ -11,6 +14,6 @@ if b > a and b > c:
     maior = b
 if c > a or c > b:
     maior = c
+
 print('O maior número é {}.'.format(maior))
 print('O menor número é {}.'.format(menor))
-# Faça um programa que leia três números e mostre qual é o maior e qual é o menor.
